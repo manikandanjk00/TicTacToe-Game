48c976e7b646146aa9115e1717c16e0cc7a7aff0
manikandanjk00 <manikandanjk00@gmail.com>
2021-07-07 10:06:51 +0530

    first comment

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import Game from "./Game.js";
+import GameView from "./GameView.js";
+
+
+let game =new Game();
+let gameView= new GameView();
+
+document.querySelector(".restart")
+.addEventListener("click", () => {
+    onRestartClick()
+});
+
+
+document.querySelectorAll(".board__tile").forEach((tile) => {
+    tile.addEventListener("click", () => {
+        onTileClick(tile.dataset.index);
+    });
+});
+// let tiles = document.querySelectorAll(".board-tile")
+// tiles.forEach((tile)=> {
+//     tile.addEventListener ("click",() => {
+//         onTileClick(tile.dataset.index);
+//     })
+// })
+
+function onTileClick(i) {
+    game.makeMove(i);
+    gameView.updateBoard(game);
+}
+
+function onRestartClick() {
+    game = new Game();
+    gameView.updateBoard(game);   
+}
+
+gameView.updateBoard(game);
